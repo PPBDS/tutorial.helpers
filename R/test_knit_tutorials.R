6a18dbcd8ee1955bd80291633b32d302d380616a
David Kane <dave.kane@gmail.com>
2023-03-09 17:09:25 -0500

    test_knit_tutorials function added

---
+++ R/test_knit_tutorials.R
@@ -0,0 +1,54 @@
+#' Knit all the tutorials in a package
+#'
+#' @param package character vector of the package name to be tested
+#'
+#' @return NULL
+#' @export
+
+test_knit_tutorials <- function(package){
+
+  # Would be nice if the function could automatically discover the library it is
+  # currently operating within.
+
+  # This testing approach only works, I think, when you click `Build -> Check`.
+  # Otherwise, the tutorials you are testing might be those you installed
+  # previously, not the ones you just edited. See discussion in
+  # all.primer.tutorials.
+
+  package_location <- system.file("tutorials", package = package)
+
+  tutorial_paths <-
+    available_tutorials(package) |>
+    mutate(path = paste0(package_location, "/", name, "/tutorial.Rmd")) |>
+    pull(path)
+
+  # stopifnot(length(tutorial_paths) >= 1)
+
+  # We use the tutorial_paths vector in the testing functions which now get run.
+
+  # Our definition of "test" for a tutorial file is to run render() and hope there
+  # is no error. There is no check to see if "tutorial.html" looks OK, just that
+  # that string is returned.
+
+  # Might we do more here? For example, what we really want to confirm is that,
+  # when a student presses the "Start Tutorial" button, things will work. I am not
+  # sure if render() is the same thing. But, the good news is that this test seems
+  # much more robust than that. In other words, it catches things that do not
+  # cause (immediate) failures with Start Tutorial.
+
+  for(i in tutorial_paths){
+    cat(paste("Testing tutorial:", i, "\n"))
+    test_that(paste("rendering", i), {
+      expect_output(rmarkdown::render(i, output_file = "tutorial.html"),
+                    "tutorial.html")
+    })
+  }
+
+  # There are two problems with this list of files. First, it is long! There are a
+  # lot of tutorials and they take a while to run. There is not a lot to be done
+  # about that. The second problem is that there is no way to customize it.
+  # Sometimes, we would just like to test one tutorial, especially while we are
+  # working on it. Other times, we want to test all the tutorials but one.
+
+  NULL
+}
