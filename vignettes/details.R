3d2047a2c001b7246f405f4137d3eb589b11e36f
David Kane <dave.kane@gmail.com>
2023-03-29 08:45:16 -0400

    get rid of details.Rmd; still wrestling with notes/warnings associated with either putting rvest in Imports or not doing that

--- vignettes/details.R
+++
@@ -1,8 +0,0 @@
-## ---- eval=FALSE--------------------------------------------------------------
-#  > .libPaths()
-#  [1] "/Users/dkane/Library/Caches/org.R-project.R/R/renv/library/all.primer.tutorials-07f29d85/R-4.2/aarch64-apple-darwin20"
-#  [2] "/Library/Frameworks/R.framework/Versions/4.2-arm64/Resources/library"
-
-## ---- echo = FALSE------------------------------------------------------------
-knitr::include_graphics("images/copy-button.png")
-
