ff1a001ec3b949fe8ebd745c872d17216a0acd64
rajarshi-mandal <rajarshi.m.1000@gmail.com>
2023-07-19 14:28:05 -0400

    Initial Commit

---
+++ R/process_submissions.R
@@ -0,0 +1,21 @@
+#' @title Lorem Ipsum
+#'
+#' @rdname Lorem Ipsum
+#'
+#' @description Lorem Ipsum
+#'
+#' @details Lorem Ipsum
+#'
+#' @param Lorem Ipsum
+#'
+#' @importFrom Lorem Ipsum
+#'
+#' @returns Lorem Ipsum
+#' 
+#' @export Lorem Ipsum
+
+knit_tutorials <- function(file_path){
+  # Read through the files
+  # Pulling out the name of the tutorial, the email of the student, and the time it took
+  # Return a tibble with that information in three variables
+}
