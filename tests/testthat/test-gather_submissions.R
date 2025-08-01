c2d7f0a708b0936a10133f403a8dc889f1a8f046
David Kane <dave.kane@gmail.com>
2025-08-01 13:36:05 -0400

    initial test cases

--- tests/testthat/test-gather_submissions.R
+++ tests/testthat/test-gather_submissions.R
@@ -26,3 +26,21 @@ test_that("gather_submissions works with single pattern", {
   # Check that all file names contain the pattern
   expect_true(all(grepl("getting", names(result))))
 })
+
+# I am suspicious of the above test cases for gather_submissions. The below test cases 
+# should be much better, because they use real answer files and cover trickier cases. 
+# Once they look good, we can probably delete the above test cases, and their input files.
+
+library(tibble)
+
+test_that("gather_submissions works with single pattern", {
+  result <- gather_submissions(path = "fixtures/answers_html/",
+                               title = "stops")
+  
+  # Check attributes of the returned list of tibbles
+  expect_true(is.list(result))
+  expect_true(length(result) == 25)
+  expect_true(all(sapply(result, tibble::is_tibble)))
+  expect_true(min(sapply(result, nrow)) == 28)
+  expect_true(max(sapply(result, nrow)) == 80)
+})
